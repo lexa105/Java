41a274be7ddadfdc48274c070a39ee58ef5bc4ec
Lexa Tuan <tuan.pham@ad24.cz>
2024-04-28 18:41:06 +0200

    pridal jsem java-playground + pro semstrálku věci.

---
+++ java_playground/src/Application.java
@@ -0,0 +1,20 @@
+public class Application {
+
+    public static void main(String[] args) {
+
+        int A;
+
+
+        char[] stars;
+
+
+
+
+        int i = 0;
+        while (i<10) {
+            System.out.println("==========================");
+            i++;
+        }
+
+    }
+}
